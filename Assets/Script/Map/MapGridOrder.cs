30fc5a5e0fb36afb03fa85647aa5d157bb959ae4
郑延 <jesonxiake@126.com>
2019-01-26 18:37:34 +0800

    测试性地加入了草皮，地图好看惹

--- Assets/Script/Map/MapGridOrder.cs
+++ Assets/Script/Map/MapGridOrder.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
-[RequireComponent(typeof(MapGridPosition), typeof(SpriteRenderer))]
+[RequireComponent(typeof(MapGridPosition))]
 [ExecuteInEditMode]
 public class MapGridOrder : MonoBehaviour {
+    public new SpriteRenderer renderer;
     private void Update() {
-        var renderer = GetComponent<SpriteRenderer>();
         var grid = GetComponent<MapGridPosition>();
-        renderer.sortingOrder = -(grid.MapPositionInt.x + grid.MapPositionInt.y) * 2;
+        if(renderer != null) {
+            renderer.sortingOrder = -(grid.MapPositionInt.x + grid.MapPositionInt.y) * 2;
+        }
     }
 }
